9a042bba406aa41ce6370ccc6c60858c7d40110a
InfinitySiggins <141016838+InfinitySiggins@users.noreply.github.com>
2023-08-01 15:48:36 +0100

    Update member.js

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var member = {
+    name: "John",
+    age: 30,
+    skills: ["JS", "React", "Node"],
+  };
+  //   console.log(member.skills[1]);
+  for (var i = 0; i < member.skills.length; i++) {
+    console.log(member.skills[i]);
+  }
+}   
